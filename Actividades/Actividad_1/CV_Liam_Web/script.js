c7a566da4b5e0b9fdcb1fe5348606576108eb996
Liamsaldana <Liam.saldana13@gmail.com>
2026-01-31 18:28:50 -0600

    feat: New Images implemented and a non functional button

--- Actividades/Actividad_1/CV_Liam_Web/script.js
+++ Actividades/Actividad_1/CV_Liam_Web/script.js
@@ -2,3 +2,10 @@
 console.log("¡Hola desde el CV de Liam! El archivo JS está conectado correctamente.");
 
 // Puedes empezar agregando algo simple aquí, como un botón que cambie el color de fondo.
+
+const boton = document.querySelector("button");
+boton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#c39b33";
+});
+
+// Aun no sirve el boton, pero lo dejamos aqui para cuando aprendamos a usarlo
